1ab050f799c396781849b2b790796ffb2b8f0fcc
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-07-10 22:29:13 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let secondCard = 7
 let sum = firstCard + secondCard
 let hasBlackJack = false // starts the play sensing that you dont have blackjack yet
 // 1. Create a variable called isAlive and assign it to true
-
+let isAlive = true
 
 // 2. Flip its value to false in the appropriate code block 
 if (sum <= 20) {
@@ -14,6 +14,8 @@ if (sum <= 20) {
     hasBlackJack = true // now the variable is true
 } else {
     console.log("Bust! 😭")
+    isAlive = false
 }
 
 // 3. Log it out to check that you're doing it right
+console.log(isAlive)
