7dca889114b6ed6abd5f481666061d10d77ce2ea
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-07-11 18:06:49 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,16 +5,17 @@ let sum = firstCard + secondCard
 let hasBlackJack = false // starts the play sensing that you dont have blackjack yet
 let isAlive = true // bust variable, left false until bust is reached 
 // 1. Declare a variable called message and assign its value to an empty string
-
+let message = ""
 // 2. Reassign the message variable to the string we're logging out
 if (sum <= 20) {
-    console.log("Do you want to draw a new card? 🙂")
+    message = "Do you want to draw a new card? 🙂"
 } else if (sum === 21) {
-    console.log("Blackjack! 🥳")
+    message = "Blackjack! 🥳"
     hasBlackJack = true // now the variable is true
 } else {
-    console.log("Bust! 😭")
+    message = "Bust! 😭"
     isAlive = false
 }
 
 // 3. Log it out!
+console.log(message)
