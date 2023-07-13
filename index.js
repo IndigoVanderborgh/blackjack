5b1c4dbf127ade66f039c79b97839364754ad1ee
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-07-12 19:19:38 -0700

    commit

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ let secondCard = 7
 let sum = firstCard + secondCard
 let hasBlackJack = false // starts the play sensing that you dont have blackjack yet
 let isAlive = true // bust variable, left false until bust is reached 
-
-
 let message = ""
-if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂"
-} else if (sum === 21) {
-    message = "Blackjack! 🥳"
-    hasBlackJack = true // now the variable is true
-} else {
-    message = "Bust! 😭"
-    isAlive = false
-}
 
-console.log(message)
+function startGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? 🙂"
+    } else if (sum === 21) {
+        message = "Blackjack! 🥳"
+        hasBlackJack = true // now the variable is true
+    } else {
+        message = "Bust! 😭"
+        isAlive = false
+    }
+    console.log(message)
+}
