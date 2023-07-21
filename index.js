c97c16a839c800cc9c7240454e286606fc94b963
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-07-20 19:14:43 -0700

    aaa

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ let sumEl = document.getElementById("sum-el")
 
 function startGame() {
     // 3. Render the sum on the page using this format -> "Sum: 14"
+    sumElEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card? 🙂"
     } else if (sum === 21) {
