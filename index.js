67c5f956269daa6b62c1451ebb8839ddceaf9ccc
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-07-20 19:09:54 -0700

    commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let isAlive = true // bust variable, left false until bust is reached
 let message = "" // message is already set
 let messageEl = document.getElementById("message-el")
 // 2. Store the sum paragraph in a variable called sumEl
-
+let sumEl = document.getElementById("sum-el")
 
 
 function startGame() {
