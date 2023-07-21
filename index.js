f2fe3a9a67a4146d2a19354eb899e33794b6df14
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-07-20 19:18:02 -0700

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ let messageEl = document.getElementById("message-el")
 // 2. Store the sum paragraph in a variable called sumEl
 let sumEl = document.getElementById("sum-el")
 // Additional knowledge: using query, we can mimic line 10 but point broadly to the id.
+// Hashtag uses id, class uses dot.
 // let sumEl = document.querySelector("#sum-el")
+// let sumEl = document.querySelector(".sum-el")
 
 function startGame() {
     // 3. Render the sum on the page using this format -> "Sum: 14"
