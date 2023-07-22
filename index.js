51dc344a7393c94e22a8f0156ce9a10bd47e4d9f
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-07-22 16:41:40 -0700

    aaa

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ let hasBlackJack = false // starts the play sensing that you dont have blackjack
 let isAlive = true // bust variable, left false until bust is reached 
 let message = "" // message is already set
 let messageEl = document.getElementById("message-el")
-// 2. Store the sum paragraph in a variable called sumEl
 let sumEl = document.getElementById("sum-el")
 // Additional knowledge: using query, we can mimic line 10 but point broadly to the id.
 // Hashtag uses id, class uses dot.
 // let sumEl = document.querySelector("#sum-el")
 // let sumEl = document.querySelector(".sum-el")
 
+
+
 function startGame() {
-    // 3. Render the sum on the page using this format -> "Sum: 14"
     sumElEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card? 🙂"
